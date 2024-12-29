b279c69c57c8885aa78a50eac5e8ef71d53f9b0a
pras <me@prasun.net>
2024-12-29 17:38:57 +0530

    implemented the app with react context api, seperated add todo to a separate component

---
+++ src/components/TodoList/index.js
@@ -0,0 +1,54 @@
+import { useState, useEffect, useContext } from 'react';
+import { TodoContext } from '../../store';
+import { APP } from '../../config';
+import { Todo } from '../Todo';
+
+export function TodoList() {
+  const {todoList, setTodoList} = useContext(TodoContext);
+
+  const markDone = (e) => {
+    let updatedList = [...todoList];
+    updatedList[e.target.dataset['index']]['done'] = e.target.checked;
+    if (e.target.checked) {
+      const checked = updatedList[e.target.dataset['index']];
+      updatedList.splice(e.target.dataset['index'], 1);
+      updatedList = [...updatedList, checked];
+    }
+    setTodoList(updatedList);
+    window.localStorage.setItem(APP.BROWSER_STORAGE_KEY, JSON.stringify(updatedList));
+  }
+
+  const deleteTodo = (e) => {
+    const updatedList = [...todoList];
+    updatedList.splice(e.target.dataset['index'], 1);
+    setTodoList(updatedList);
+    window.localStorage.setItem(APP.BROWSER_STORAGE_KEY, JSON.stringify(updatedList));
+  }
+
+  const deleteTodoList = () => {
+    setTodoList([]);
+    window.localStorage.setItem(APP.BROWSER_STORAGE_KEY, JSON.stringify([]));
+  }
+
+  return (
+    <div className='appContainer'>
+      <Todo />
+      <div className='todoListContainer'>
+        <ul className='todoList'>
+          {todoList && todoList.length > 0 && todoList.map((todo, i) => (
+            <li key={i} className='listItem'>
+              <div className='itemCheckboxContainer'>
+                <input type='checkbox' checked={todo?.done} data-index={i} onChange={markDone} />
+              </div>
+              <div className={`itemText ${todo?.done ? 'done' : ''}`}>{todo?.text}</div>
+              <div className='itemActionsContainer'>
+                <button className='deleteItem' onClick={deleteTodo} data-index={i}>X</button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <button className='deleteAll' onClick={deleteTodoList}>Delete All</button>
+    </div>
+  );
+}
